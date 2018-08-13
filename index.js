076a4ff440cc8b812b14234752b7a2aed8550930
Daniel <dk@g9h.co>
2018-08-13 15:08:33 +0200

    prettier

--- index.js
+++ index.js
@@ -10,51 +10,50 @@
  * @param  {mixed}  what  Something
  * @return  {boolean}  `true` if it is an object
  */
-const isObject = what => null !== what && ! Array.isArray(what) && 'object' === typeof what;
+const isObject = what => null !== what && !Array.isArray(what) && 'object' === typeof what;
 
- /**
-  * Flattens a nested object up to the first level.
-  * Names are joined with the given string, '_' by default.
-  *
-  * Example:
-  * ------BEFORE---------+---------AFTER-----------
-  * {                    | {
-  *   Parent: {          |    Parent_Child1() {
-  *     Child1() {       |    },
-  *     },               |
-  *                      |    Parent_Child2() {
-  *     Child2() {       |    },
-  *     },               |
-  *   },                 |    Firstlevel() {
-  *                      |    }
-  *   Firstlevel() {     |
-  *   }                  | }
-  * }                    |
-  *
-  * @param  {object}  target  Object to flatten
-  * @param  {string=}  glue  Combining string, default '_'
-  * @return  {object}  Flattened object
-  */
+/**
+ * Flattens a nested object up to the first level.
+ * Names are joined with the given string, '_' by default.
+ *
+ * Example:
+ * ------BEFORE---------+---------AFTER-----------
+ * {                    | {
+ *   Parent: {          |    Parent_Child1() {
+ *     Child1() {       |    },
+ *     },               |
+ *                      |    Parent_Child2() {
+ *     Child2() {       |    },
+ *     },               |
+ *   },                 |    Firstlevel() {
+ *                      |    }
+ *   Firstlevel() {     |
+ *   }                  | }
+ * }                    |
+ *
+ * @param  {object}  target  Object to flatten
+ * @param  {string=}  glue  Combining string, default '_'
+ * @return  {object}  Flattened object
+ */
 const flatten = (target, glue) => {
-    'undefined' !== typeof glue || ( glue = '_' );
+    'undefined' !== typeof glue || (glue = '_');
     const output = {};
     const keys = Object.keys(target);
 
-    keys.forEach( key => {
+    keys.forEach(key => {
         const sub = target[key];
 
         // If it is not an object, copy as is
-        if( ! isObject(sub) ) {
+        if (!isObject(sub)) {
             output[key] = sub;
             return;
         }
 
         // We expect this to be a child element
-        Object.keys(sub)
-                .forEach( subkey => {
-                    const name = [key, glue, subkey].join('');
-                    output[name] = sub[subkey];
-                });
+        Object.keys(sub).forEach(subkey => {
+            const name = [key, glue, subkey].join('');
+            output[name] = sub[subkey];
+        });
     });
 
     return output;
@@ -74,12 +73,11 @@ module.exports = (reducers, initial, glue) => {
 
     // The reducer
     return (state, action) => {
-        state || ( state = initial );
+        state || (state = initial);
         const type = action.type;
 
         // Reducer for that action given?
-        if( 'function' === typeof actual[type] )
-            return actual[type](state, action);
+        if ('function' === typeof actual[type]) return actual[type](state, action);
 
         return state;
     };
